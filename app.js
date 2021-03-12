b89e6b871d037bcbc1f16400332b62c1e94340e5
sandeepnadendla-git <77738760+sandeepnadendla-git@users.noreply.github.com>
2021-03-12 12:29:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server =
     (req,res)=> {               //Anonymous function to handle the request
       res.statusCode = 200      //code for OK
       res.setHeader('Content-Type', 'text/plain') //Set the mime type
-      res.end('The first job i wanted when you were a little kid is dog catcher')    //Close the response and provide content
+      res.end('The first job i wanted when i was a little kid is dog catcher')    //Close the response and provide content
     }                           //No return needed, we modified the res object we got
 )
 
